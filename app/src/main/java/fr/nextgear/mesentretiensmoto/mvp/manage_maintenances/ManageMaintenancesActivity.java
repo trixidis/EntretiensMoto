41178d4df5666c5b8c829be9b928e51fa46a6e7c
Adrien Boissier <adrien.boissier_ext@michelin.com>
2017-11-22 13:30:51 +0100

    modification du Material view pager header

--- app/src/main/java/fr/nextgear/mesentretiensmoto/mvp/manage_maintenances/ManageMaintenancesActivity.java
+++ app/src/main/java/fr/nextgear/mesentretiensmoto/mvp/manage_maintenances/ManageMaintenancesActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.github.florent37.materialviewpager.MaterialViewPager;
 
@@ -18,20 +20,39 @@ import se.emilsjolander.intentbuilder.IntentBuilder;
 @IntentBuilder
 public class ManageMaintenancesActivity extends AppCompatActivity implements FragmentManageMaintenances.GetBikeFromActivityCallback {
 
+    //region Attributes
+
     @Extra
     Bike mBike;
+
+    //endregion Attributes
+
+    //region Bind Views
+
     @BindView(R.id.materialViewPager)
     MaterialViewPager mViewPager;
+
+    //endregion Bind Views
+
+    //region Lifecycle Methods
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        removeNotificationBarAndSetFullscreen();
         setContentView(R.layout.activity_manage_maintenances);
         ButterKnife.bind(this);
         ManageMaintenancesActivityIntentBuilder.inject(getIntent(), this);
-        ActionBar loActionBar = getSupportActionBar();
-        loActionBar.setTitle(R.string.title_activity_manage_maintenances);
-        loActionBar.setSubtitle(mBike.nameBike);
+        setupViewPager();
+    }
+
+    //endregion Lifecycle Methods
+
+    //region private Methods
+
+    private void setupViewPager() {
+        mViewPager.getToolbar().setTitle(R.string.title_activity_manage_maintenances);
+        mViewPager.getToolbar().setSubtitle(mBike.nameBike);
         mViewPager.getViewPager().setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public int getCount() {
@@ -40,18 +61,24 @@ public class ManageMaintenancesActivity extends AppCompatActivity implements Fra
 
             @Override
             public Fragment getItem(int position) {
-                return new FragmentManageMaintenances() ;
+                return new FragmentManageMaintenances();
             }
         });
-       /* FragmentManageMaintenances fragment = new FragmentManageMaintenances();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.ManageMaintenancesActivity_FrameLayout_Content, fragment)
-                .commit();*/
     }
+
+    private void removeNotificationBarAndSetFullscreen() {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+    }
+
+    //endregion private Methods
+
+    //region Interfaces implementation
 
     @Override
     public Bike getCurrentSelectedBike() {
         return mBike;
     }
+
+    //endregion Interfaces implementation
 }
